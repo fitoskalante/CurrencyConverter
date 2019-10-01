c70cdedb6e656e765162dafab5f98468e0fe5e11
Ricardo Cuevas Escalante <ricardocuevasescalante@Ricardos-MacBook-Pro.local>
2019-10-01 18:22:48 +0700

    updating css style

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,3 @@
-const exchangeRates = {
-    "usd": {
-        "eur": 0.91,
-        "aud": 1.48,
-        "krw": 1203.00,
-        "vnd": 23200.70,
-    },
-    "eur": {
-        "usd": 1.09,
-        "aud": 1.62,
-        "krw": 1316.21,
-        "vnd": 25383.96,
-    },
-    "aud": {
-        "usd": 0.68,
-        "eur": 0.62,
-        "krw": 813.58,
-        "vnd": 15690.63,
-    },
-    "krw": {
-        "usd": 0.00083,
-        "aud": 0.0012,
-        "eur": 0.00076,
-        "vnd": 19.29,
-    },
-    "vnd": {
-        "krw": 0.052,
-        "usd": 0.000043,
-        "aud": 0.000064,
-        "eur": 0.000039,
-    },
-}
-
 function formatCurrency(type, value) {
     const formatter = new Intl.NumberFormat(type, {
         currency: type,
@@ -42,7 +9,6 @@ function formatCurrency(type, value) {
 async function convertCurrency() {  
     const from = fromSelector.value
     const to = toSelector.value
-    // const convertedValue = exchangeRates[from][to] * inputAmount.value
     const convertedValue = await callApi(from, to, inputAmount.value)
     result.innerHTML = formatCurrency(to, convertedValue)
 }
@@ -52,7 +18,6 @@ let inputAmount = document.getElementById('amountToConvert')
 let result = document.getElementById('result')
 let fromSelector = document.getElementById('currencyFrom')
 let toSelector = document.getElementById('currencyTo')
-// converterButton.addEventListener('click', convertCurrency)
 
 inputAmount.addEventListener('input', convertCurrency)
 
@@ -69,3 +34,5 @@ async function callApi(fromcurrency, tocurrency, amount) {
 function updateResults(response) {
   console.log(response);
 }
+
+console.log('testing code', convertCurrency)
